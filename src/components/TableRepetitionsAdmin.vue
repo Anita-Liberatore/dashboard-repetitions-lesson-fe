7d184aea2da076fb17a7d50d44817d31ebdae195
Anita Liberatore <anitaliberatore@hotmail.com>
2022-07-01 00:20:26 +0200

    WIP

---
+++ src/components/TableRepetitionsAdmin.vue
@@ -0,0 +1,113 @@
+<template>
+    <div class="flex flex-col md:flex-row ml-3 lg:ml-10 mt-3">
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            Nome docente
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Nome corso
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Stato lezione
+                        </th>
+
+                        <th scope="col" class="px-6 py-3">
+                            Orario
+                        </th>
+
+                        <th scope="col" class="px-6 py-3">
+                            Giorno
+                        </th>
+
+                        <th scope="col" class="px-6 py-3">
+                            Username
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <tr class="odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700" :key="x.id"
+                        v-for="x in repetitions">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                            {{ x.nameProfessor + " " + x.surnameProfessor }}
+                        </th>
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                            {{ x.courseName }}
+                        </th>
+
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                            <div class="label-et">
+                                <span v-if="x.status === 'P'"
+                                    class="bg-yellow-100 text-yellow-800 text-xs font-semibold rounded dark:bg-yellow-200 dark:text-yellow-900">PRENOTATA</span>
+                            </div>
+                            <div class="label-et">
+                                <span v-if="x.status === 'E'"
+                                    class="bg-green-100 text-green-800 text-xs font-semibold rounded dark:bg-green-200 dark:text-green-900">EFFETTUATA</span>
+                            </div>
+
+                            <div class="label-et">
+                                <span v-if="x.status === 'D'"
+                                    class="bg-red-100 text-red-800 text-xs font-semibold rounded dark:bg-red-200 dark:text-red-900">DISDETTA</span>
+                            </div>
+                        </th>
+
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                            {{ x.hour }}
+                        </th>
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                            {{ x.day == 'L' ? 'Lunedì' : x.day == 'M' ? 'Martedì' : x.day == 'ME' ?
+                                    'Mercoledì' : x.day == 'G' ?
+                                        'Giovedì' : x.day == 'V' ? 'Venerdì' : ''
+                            }}
+                        </th>
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                            {{ x.user }}
+                        </th>
+                        <td class="px-6 py-4 text-right">
+                            <button type="button" v-if="x.status === 'P'"
+                                class="my-red text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                                @click="$emit('delete-repetition', x.id)">Elimina</button>
+                        </td>
+                        
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+
+
+
+</template>
+
+<script>
+export default {
+
+    data() {
+        return {
+        }
+    },
+
+    emits: ["delete-repetition"],
+    props: {
+        repetitions: Object,
+    },
+
+    methods: {
+
+    }
+}
+
+</script>
+
+<style>
+.my-red {
+    background-color: #ff595e;
+}
+</style>
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -33,7 +33,7 @@
                 <i class="fa-solid fa-arrows-left-right-to-line fa-2xl"></i>
             </div>
 
-            <div @click=""
+            <div @click="tabRepetitions()"
                 class="flex justify-between items-center bg-[#bee3db] w-[94vw] md:w-[220px] lg:w-[200px] xl:w-[250px] px-6 py-8 rounded-2xl md:ml-7 md:mt-0 mt-4">
                 <button>
                     <p class="font-normal text-xl">Lista prenotazioni</p>
@@ -57,9 +57,14 @@
                 @add-professor="addProfessor" />
         </div>
 
-         <div v-if="tab == 3">
-            <p class="font-normal text-xl  px-12 py-5 pt-8">Lista Docenti</p>
-            <TableAssociation :associations="associations" @delete-association="deleteAssociation"/>
+        <div v-if="tab == 3">
+            <p class="font-normal text-xl  px-12 py-5 pt-8">Lista Associazioni corso-docente</p>
+            <TableAssociation :associations="associations" @delete-association="deleteAssociation" />
+        </div>
+
+        <div v-if="tab == 4">
+            <p class="font-normal text-xl  px-12 py-5 pt-8">Lista Ripetizioni</p>
+            <TableRepetitionsAdmin :repetitions="repetitions" @delete-repetition="deleteRepetition" />
         </div>
     </section>
 </template>
@@ -69,13 +74,15 @@ import UserDetail from "../components/UserDetail.vue";
 import TableCourse from "../components/TableCourse.vue";
 import TableProfessor from "../components/TableProfessor.vue";
 import TableAssociation from "../components/TableAssociation.vue";
+import TableRepetitionsAdmin from "../components/TableRepetitionsAdmin.vue";
 export default {
     data() {
         return {
             tab: 0,
             courses: [],
             professors: [],
-            associations: []
+            associations: [],
+            repetitions: []
         };
     },
     computed: {
@@ -106,12 +113,41 @@ export default {
             return data;
         },
 
-         async fetchAssociations() {
+        async fetchRepetitions() {
+            const res = await fetch(
+                "http://localhost:8080/backend-unito-extraprof/repetitions-admin"
+            );
+            const data = await res.json();
+            return data;
+        },
+        async fetchAssociations() {
             const res = await fetch(
                 `http://localhost:8080/backend-unito-extraprof/associazioni`
             );
             const data = await res.json();
             return data;
+        },
+
+        async deleteRepetition(id) {
+            const data = {
+                status: 'D'
+            }
+
+            console.log(data)
+
+            const res = await fetch(
+                `http://localhost:8080/backend-unito-extraprof/repetitions-delete?id=${id}`,
+                {
+                    method: "POST",
+                    mode: "no-cors", // 'cors' by default,
+                    headers: {
+                        "Content-type": "application/json",
+                    },
+                    body: JSON.stringify(data),
+                }
+            );
+            this.repetitions = await this.fetchRepetitions();
+
         },
 
         async deleteProfessor(id) {
@@ -169,8 +205,13 @@ export default {
         },
 
         async tabAssociations() {
-           this.tab = 3;
-           this.associations = await this.fetchAssociations();
+            this.tab = 3;
+            this.associations = await this.fetchAssociations();
+        },
+
+        async tabRepetitions() {
+            this.tab = 4;
+            this.repetitions = await this.fetchRepetitions();
         },
 
         async deleteCourse(id) {
@@ -208,6 +249,6 @@ export default {
             alert("Hai inserito un corso!")
         },
     },
-    components: { UserDetail, TableCourse, TableProfessor, TableAssociation }
+    components: { UserDetail, TableCourse, TableProfessor, TableAssociation, TableRepetitionsAdmin }
 }
 </script>
